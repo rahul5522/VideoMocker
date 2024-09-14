fe67a18606e771883489366c70cc819d36a8fd94
rahul5522 <rahulwaghmare552@gmail.com>
2024-09-14 14:35:23 +0530

    Audio removed

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ app.post('/api/generate-video', async (req, res) => {
       '-y',  // Overwrite output file if it exists
       '-f', 'lavfi',
       '-i', `color=c=blue:s=${width}x${height}:d=${duration}`,
-      '-i', `sine=frequency=440:duration=${duration}`,
+      // '-i', `sine=frequency=440:duration=${duration}`,
       '-vf', `drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:fontsize=60:fontcolor=white:x=(w-tw)/2:y=(h-th)/2:text='${width}x${height}'`,
       '-t', duration,
       '-c:v', 'libx264',
